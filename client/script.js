ff7c9e023fcb59c4b89c9067757247b83ffb1008
Om soni <omsoni774600@gmail.com>
2023-08-17 00:50:22 +0530

    updates url

--- client/script.js
+++ client/script.js
@@ -91,7 +91,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('https://tars-ai.onrender.com', {
+    const response = await fetch('https://tars-40p2qot3p-omsoni06.vercel.app', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
